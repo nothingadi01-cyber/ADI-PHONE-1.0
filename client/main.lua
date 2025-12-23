9dbac2a49e10b10c7101eec30a262ca77a2c6bd5
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-23 12:36:08 +0530

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -24,3 +24,32 @@ RegisterNUICallback("getCryptoPrice", function(data, cb)
     local btcPrice = math.random(40000, 60000)
     cb({ price = btcPrice })
 end)
+local currentCamera = "rear"
+local fov = 50.0
+
+-- SWITCH FRONT/REAR CAMERA
+function switchCamera()
+    if currentCamera == "rear" then
+        currentCamera = "front"
+        AttachCamToEntity(phoneCam, PlayerPedId(), 0.05, 0.5, 0.8, true) -- Front view
+    else
+        currentCamera = "rear"
+        AttachCamToEntity(phoneCam, PlayerPedId(), 0.1, 0.1, 0.8, true) -- Normal view
+    end
+end
+
+-- CAMERA ZOOM LOGIC
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if isCameraOpen then
+            if IsControlJustPressed(0, 27) then -- Scroll Up
+                fov = math.max(10.0, fov - 5.0)
+                SetCamFov(phoneCam, fov)
+            elseif IsControlJustPressed(0, 173) then -- Scroll Down
+                fov = math.min(70.0, fov + 5.0)
+                SetCamFov(phoneCam, fov)
+            end
+        end
+    end
+end)
