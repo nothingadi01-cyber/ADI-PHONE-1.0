6f84bea731204740d5e1af3b515bb29a5194d7bd
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-23 18:37:08 +0530

    Update broadcast.lua

--- client/broadcast.lua
+++ client/broadcast.lua
@@ -11,3 +11,33 @@ function startPodcast()
     local micProp = CreateObject(`p_ingame_mic_01`, 0, 0, 0, true, true, true)
     AttachEntityToEntity(micProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 60309), 0.05, 0.05, 0.0, 0.0, 0.0, 0.0, true, true, false, true, 1, true)
 end
+
+local isBroadcasting = false
+
+function toggleLive()
+    isBroadcasting = not isBroadcasting
+    local ped = PlayerPedId()
+
+    if isBroadcasting then
+        -- Character holds phone up like a pro camera
+        TaskStartScenarioInPlace(ped, "WORLD_HUMAN_MOBILE_FILM_PHOTOGRAPH", 0, true)
+        
+        -- Start the Stream
+        local tickerText = GetTickerInput()
+        TriggerServerEvent('adi_phone:server:startGlobalBroadcast', tickerText)
+    else
+        ClearPedTasks(ped)
+        TriggerServerEvent('adi_phone:server:stopGlobalBroadcast')
+    end
+end
+
+-- Replacing textures on every TV in the world
+RegisterNetEvent('adi_phone:client:syncTvFeed')
+AddEventHandler('adi_phone:client:syncTvFeed', function(isLive, url)
+    if isLive then
+        AddReplaceTexture("prop_tv_flat_01", "tv_screen", "AdiNews", "LiveFeed")
+        -- Logic to point the DUI to the URL
+    else
+        RemoveReplaceTexture("prop_tv_flat_01", "tv_screen")
+    end
+end)
