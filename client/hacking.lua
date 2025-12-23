97e95636b53267a99b5389a6bbc6c93161161529
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-23 14:52:36 +0530

    Update hacking.lua

--- client/hacking.lua
+++ client/hacking.lua
@@ -13,3 +13,36 @@ RegisterNUICallback("hackTrafficLight", function(data, cb)
     end
     cb('ok')
 end)
+
+RegisterNUICallback("attemptVehicleHack", function(data, cb)
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 3.0, 0, 71)
+
+    if DoesEntityExist(vehicle) then
+        -- Character Animation: Leaning over the dashboard/door
+        TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_WELDING", 0, true)
+        
+        -- Trigger the NUI Hacking Mini-game
+        SendNUIMessage({ action = "startHackGame", difficulty = "hard" })
+    else
+        SendNUIMessage({ action = "adi_voice", msg = "NO VEHICLE SIGNAL DETECTED." })
+    end
+    cb('ok')
+end)
+
+-- On Successful Hack
+RegisterNetEvent('adi_phone:hackSuccess')
+AddEventHandler('adi_phone:hackSuccess', function(netId)
+    local vehicle = NetToVeh(netId)
+    SetVehicleDoorsLocked(vehicle, 1) -- Unlock
+    SetVehicleNeedsToBeHotwired(vehicle, false)
+    SetVehicleEngineOn(vehicle, true, true, false)
+    
+    -- Visual Feedback: Lights Flash
+    SetVehicleLights(vehicle, 2)
+    Wait(500)
+    SetVehicleLights(vehicle, 0)
+    
+    SendNUIMessage({ action = "adi_voice", msg = "IGNITION BYPASSED. WELCOME, OPERATOR." })
+end)
