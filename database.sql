b6a5f92873e978751a8610f286260e5f2d7d3681
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-23 19:41:38 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `phone_settings` (
+    `citizenid` VARCHAR(50) PRIMARY KEY,
+    `background` TEXT DEFAULT 'default.jpg',
+    `ringtone` VARCHAR(255) DEFAULT 'standard.mp3',
+    `dark_mode` TINYINT(1) DEFAULT 1,
+    `airplane_mode` TINYINT(1) DEFAULT 0,
+    `encryption` TINYINT(1) DEFAULT 0
+);
